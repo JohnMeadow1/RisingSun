c7207adf73365a35a2d8e4ae0d3e4b50d5928a2c
Ryjek <rakfrag@gmail.com>
2018-05-26 00:39:49 +0200

    Add players; Add drag-item machanics;

---
+++ source/scripts/players/player1.gd
@@ -0,0 +1,72 @@
+extends Node2D
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+# Const
+const MOVE_SPEED = 200 
+
+const BOOST_TIME = 0.4
+const BOOST_SPEED = 200
+
+export(int) var PLAYER_NUM
+
+var boost_timer = null
+var drag_item = null
+
+func _ready():
+	# Called every time the node is added to the scene.
+	# Initialization here
+	
+	# Setup timer for boost  
+	self.boost_timer = Timer.new()
+	self.boost_timer.wait_time = BOOST_TIME
+	self.boost_timer.set_one_shot(true)
+	self.add_child(self.boost_timer)
+	
+func _process(delta):
+		
+	#
+	# Move inputs
+	#
+	var offset = (MOVE_SPEED + (self.boost_timer.time_left / self.boost_timer.wait_time) * BOOST_SPEED) * delta
+	
+	if Input.is_action_pressed("move_up_p" + str(PLAYER_NUM)) and position.y > 0:
+		position.y -= offset;
+
+	if Input.is_action_pressed("move_down_p" + str(PLAYER_NUM)) and position.y < get_viewport_rect().size.y:
+		position.y += offset;
+
+	if Input.is_action_pressed("move_left_p" + str(PLAYER_NUM)) and position.x > 0:
+		position.x -= offset;
+
+	if Input.is_action_pressed("move_right_p" + str(PLAYER_NUM)) and position.x < get_viewport_rect().size.x:
+		position.x += offset;
+
+	# Boost
+	if Input.is_action_pressed("alt_p" + str(PLAYER_NUM)) and position.x < get_viewport_rect().size.x:
+		self.boost_timer.start()
+
+	#
+	# Others
+	#
+	
+	# Pick item on the floor
+	if Input.is_action_just_pressed("use_p" + str(PLAYER_NUM)):
+		if self.drag_item == null:
+			# Hands empty - drag item
+			for item in $"../items".get_children():
+				if ( (item.position - position).length() < 30 ) and ( item.dragged == false):
+					item.dragged = true
+					self.drag_item = item
+		else:
+			# Hands occupied - left item
+			self.drag_item.dragged = false
+			self.drag_item = null
+				
+	# Update position of picked item
+	if self.drag_item != null:
+		self.drag_item.position = self.position
+		
+	
