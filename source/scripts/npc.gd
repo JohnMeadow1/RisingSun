1d74808adb0c01c0745161fcafc1cf27ba5ef20e
Ryjek <rakfrag@gmail.com>
2018-05-26 02:37:18 +0200

    Improve NPC AI; Fix animations;

--- source/scripts/npc.gd
+++ source/scripts/npc.gd
@@ -1,39 +1,87 @@
 extends Node2D
 
-var  timer = 0.0
-var  timer_change = 1
+var walk_timer = null
+var idle_timer = null
+
+enum {STATE_IDLE, STATE_WALKING}
 
 var speed = 0
 var dir = Vector2()
-var orient = 0
 var dragged = false
+var state = STATE_IDLE
 
 func _ready():
-	
 	randomize()
+	
+	# Move AI
+	self.walk_timer = Timer.new()
+	self.walk_timer.set_one_shot(true)
+	self.add_child(self.walk_timer)
+	
+	self.idle_timer = Timer.new()
+	self.idle_timer.set_one_shot(true)
+	self.add_child(self.idle_timer)
 	
 	position.x = rand_range(50,500)
 	position.y = rand_range(50,500)
 	
+	# Init behavior
+	if randi() % 2:
+		self.randomize_move()
+	else:
+		self.randomize_idle()
+	
+func randomize_move():
+	$Sprite/AnimationPlayer.play("walk")
+	self.state = STATE_WALKING
+
+	# Timer
+	self.walk_timer.wait_time = rand_range(3.0, 5.0)
+	self.walk_timer.start()
+	
 	speed = rand_range(0.3, 1.0)
-	
-	orient = rand_range(0, 2 * PI)
-	
+	var orient = rand_range(0, 2 * PI)
 	dir = Vector2(sin(orient), cos(orient))
 
-func _physics_process(delta):
+func randomize_idle():
+	$Sprite/AnimationPlayer.play("idle")
+	self.state = STATE_IDLE
 	
-	timer += delta * 2 * timer_change
-	if timer > 1 or timer < -1:
-		timer = clamp( timer, -1, 1 )
-		timer_change = -timer_change
-		dir = -dir
+	# Timer	
+	self.idle_timer.wait_time = rand_range(1.0, 3.0)
+	self.idle_timer.start()
 	
-	position += dir * speed 
+func drag(flag):
+	self.dragged = flag
 	
-	if(dir.x > 0):
-		$Sprite.flip_h = false 
+	if flag:
+		$Sprite/AnimationPlayer.play("panic")
 	else:
-		$Sprite.flip_h = true
+		randomize_move()
+
+func _physics_process(delta):
+	if self.dragged:
+		return
 	
-	pass
+	match self.state:
+		#######################################################
+		STATE_IDLE:
+			# Stopped
+			if self.idle_timer.is_stopped():
+				self.randomize_move()
+			
+		#######################################################
+		STATE_WALKING:
+			# Walking
+			if self.walk_timer.is_stopped():
+				self.randomize_idle()
+			else:
+				if !get_viewport_rect().has_point(self.position):
+					dir = -dir
+				
+				position += dir * speed
+				
+				if(dir.x > 0):
+					$Sprite.flip_h = false 
+				else:
+					$Sprite.flip_h = true
